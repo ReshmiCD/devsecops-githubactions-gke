c650506634d45c68e5da90831aa87ec0b9d589fa
ReshmiCD <54526125+ReshmiCD@users.noreply.github.com>
2021-09-25 13:17:00 +0530

    Update zap-script.sh

--- script/zap-script.sh
+++ script/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "http://3.135.209.44:4200/" -l PASS > zap_baseline_report.html
+docker run -i owasp/zap2docker-stable zap-baseline.py -t "http://35.200.200.123/" -l PASS > zap_baseline_report.html
 
 echo $? > /dev/null
