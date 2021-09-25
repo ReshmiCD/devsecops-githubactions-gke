2c8c247c6c2222d3c37e2cde74cecb05d434053d
ReshmiCD <54526125+ReshmiCD@users.noreply.github.com>
2021-09-25 21:34:03 +0530

    Delete .github/script directory

--- .github/script/zap-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "http://35.200.200.123/" -l PASS > zap_baseline_report.html
-
-echo $? > /dev/null
